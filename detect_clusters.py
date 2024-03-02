ee2ac7e2853bef8df83f5ff79b0be66b60a261ee
Gili Wolf <giliwof1999@gmail.com>
2024-03-02 17:59:17 +0200

    detect_clusters works

---
+++ detect_clusters.py
@@ -0,0 +1,78 @@
+import pysam
+import csv
+import sys
+import os
+# cigartuples
+
+bam_path = "/private10/Projects/Gili/HE_workdir/first_part/name_test/second_map/A2C/SRR11548778_A2C_Aligned.out.bam"
+fasta_path = "/private10/Projects/Gili/HE_workdir/genome_setup/hg38.fa"
+
+output_path = "/private10/Projects/Gili/HE_workdir/detection/detect_clusters_test/SRR11548778_A2C_output.csv"
+
+ref_base = 'A'
+alt_base = 'C'
+
+# Open the BAM file for reading
+bam_file = pysam.AlignmentFile(bam_path, "rb")
+
+# Open the FASTA genome file
+fasta_file = pysam.FastaFile(fasta_path)
+
+# Open the output file for writing
+output_file = open(output_path, "w", newline='')
+csv_writer = csv.writer(output_file)
+
+# Write the header row to the CSV file
+header = ['Read_ID', 'Chromosome', 'Position','Alignment_length', 'Reference_Sequence', 'cigar','blocks', 'HE_Sequence', 'Number_of_MM', 'Number_of_Editing_Sites', 'Editing_Sites_List']
+csv_writer.writerow(header)
+
+# Iterate through each read in the BAM file
+for read in bam_file:
+    # Check if the read is mapped
+    if not read.is_unmapped:
+        # Get the read's mapped region coordinates 
+        chromosome = bam_file.get_reference_name(read.reference_id)  # chromosome name
+        position = read.reference_start  # start position of the allignment
+        
+        # Extract the sequence from the genome:
+        # get the cigar
+        cigar = read.cigarstring
+        # get blocks of alligned regions (if size of blocks > 1 -> the read is spliced)
+        blocks = read.get_blocks()
+        reference_sequence=""
+        # iterate over the blocks of refrence sequence using block[0](start position) and block[1](end position)
+        for block in blocks:
+            reference_sequence += str(fasta_file.fetch(chromosome, block[0], block[1])).upper()
+        
+        allignment_length = len(reference_sequence)
+
+        # get the sequence of the read itself.
+        # if reversed - get the reverse complemented (get_forward_sequence)
+        HE_sequence = read.get_forward_sequence() if read.is_reverse else read.query_alignment_sequence
+
+        # list of the quality of each base
+        quality = read.query_alignment_qualities
+        
+        num_of_mm = 0
+        num_of_editing_sites = 0
+        
+        # map : (position: quality)
+        editing_sites = {}
+        # Extract MM and editing sites:
+        for i in range(min(len(reference_sequence), len(HE_sequence), len(quality))):
+            temp_ref_base = reference_sequence[i]
+            temp_alt_base = HE_sequence[i]
+            if temp_ref_base != temp_alt_base:
+                num_of_mm += 1
+                if (temp_ref_base == ref_base) and (temp_alt_base == alt_base):
+                   num_of_editing_sites += 1
+                   editing_sites[i] = quality[i]
+
+
+        # Write the parameters to the CSV file
+        # 'Read_ID', 'Chromosome', 'Position','Alignment_length', 'Reference_Sequence', 'cigar','blocks', 'HE_Sequence', 'Number_of_MM', 'Number_of_Editing_Sites', 'Editing_Sites_List'
+        csv_writer.writerow([read.query_name, chromosome, position, allignment_length, reference_sequence,cigar, blocks, HE_sequence, num_of_mm, num_of_editing_sites, editing_sites])
+
+# Close the BAM and FASTA files
+bam_file.close()
+fasta_file.close()
