8e7360e3ac523176ab76eb12bbcbf11ebee278cf
GiliWolf <106915399+GiliWolf@users.noreply.github.com>
2024-05-02 13:35:17 +0300

    Update filter_clusters.py
    added json ouyput summary. finished

--- filter_clusters.py
+++ filter_clusters.py
@@ -11,20 +11,31 @@ This script is designed to filter HE read based on pre-defined conditions:
     (-) number of editing sites to read's length ratio
     (-) density of the clusters (length of the cluster to read's length ratio)
 
-the script output 2 CSV file: 
-1 - passed csv with the information of the reads that passed all of the consitions 
-2 - condition_analysis CSV file with one-hot encoded analysis for each of the conditions
+the script output 3 files (if -t == 'all'): 
+1 - CSV with the information of the reads that passed all of the consitions 
+2 - CSV file with True/False for each of the conditions (passes/not passed) for each read
+3 - JSON summary file of the sample
 ----------------------------
 
 Usage:
-    filter_clusters.py -i <SAMPLE_CLUSTERS_CSV> [-o <passed_CSV_PATH> -O <CONDITION_ANALYSIS_PATH>] [Options..]
+    filter_clusters.py -i <SAMPLE_CLUSTERS_CSV> \
+                       [-o <passed_CSV_PATH> -O <CONDITION_ANALYSIS_PATH> -id <SAMPLE_ID> \
+                        -j <JSON_PATH> -t {all,passed,analysis,summary}] \
+                       [Parameters Options..]
 
+Arguments: 
+REQUIRED-
+input files:
+    -i --input                      path to the read's detected clusters (output of detect_clusters.py)
+
+OPTIONAL-
 output files:
-    -o --passed_output            path to the read's passed output.
+    -id --sample_id                 sample ID
+    -o --passed_output              path to the read's passed output.
     -O --detailed_condition_output  path to the read's csv file with all the conditions anaylsis output
-    -t --output_types"              choose the type of output: 'all', 'passed', 'analysis'
-
-optional arguments:
+    -j --json_summary               path to the sample's summary json file
+    -t --output_types               choose the type of output: 'all', 'passed', 'analysis', 'summary'
+filter parameters:
   -es --min_editing_sites           MIN_EDITING_SITES
                                     minimum number of editing sites.
 
@@ -48,6 +59,8 @@ import sys
 import argparse
 from itertools import combinations
 from collections import Counter
+import json
+
 
 #Controling Arguments:
 parser = argparse.ArgumentParser(description="This script is designed to filter HE read based on pre-defined conditions.")
@@ -67,9 +80,12 @@ parser.add_argument("-es2l", "--min_es_length_ratio", dest ="min_es_length_ratio
 parser.add_argument("-cl2l", "--min_cluster_length_ratio", dest ="min_cluster_length_ratio", type=float, required=False, default=0.1, help="minimum ratio of: cluster's length to read's length")
 
 # OPTIONAL: control output files
+parser.add_argument("-id", "--sample_id",dest = "sample_id", required=False, type=str, default="sample", help="Sample ID.")
 parser.add_argument("-o", "--passed_output", dest ="passed_csv_path", type=str, required=False, default = "passed.csv", help="path to the read's passed output.")
 parser.add_argument("-O", "--detailed_condition_output", dest ="condition_analysis_path", type=str, required=False, default = "condition_analysis.csv", help="path to the read's csv file with all the condtions detailed output.")
-parser.add_argument("-t", "--output_types", dest="output_types", choices=["all", "passed", "analysis"], default="all", help="choose the type of output: 'all', 'passed', 'analysis'")
+parser.add_argument("-j", "--json_summary", dest="json_path", type=str, required=False,default = "summary.json", help="path to the sample's summary json file.")
+parser.add_argument("-t", "--output_types", dest="output_types", choices=["all", "passed", "analysis", "summary"], default="all", help="choose the type of output: 'all', 'passed', 'analysis', 'summary'")
+
 
 args = parser.parse_args()
 
@@ -80,6 +96,7 @@ args = parser.parse_args()
 sample_clusters_csv = str(args.sample_clusters_csv).strip()
 passed_csv_path = str(args.passed_csv_path).strip()
 condition_analysis_path = str(args.condition_analysis_path).strip()
+json_summary_path = str(args.json_path).strip()
 
 # Initialize lists to store rows
 passed_reads_data = []
@@ -88,17 +105,74 @@ condition_analysis_rows = []
 # read data from the detected clusters file
 clusters_df = pd.read_csv(sample_clusters_csv, index_col=0)
 
-# output types - 
+# output types -
+sample_id = args.sample_id
 out_passed = args.output_types == 'all' or args.output_types == 'passed'
 out_analysis = args.output_types == 'all' or args.output_types == 'analysis'
+out_json = args.output_types == 'all' or args.output_types == 'summary'
+out_json = True
+# script parameters 
+motif_location = ['upstream', 'downstream']
 
 # True if consition passed, False if did not
 def check_Condition(value, threshold):
     return value >= threshold
 
+def sample_motif(nt_count_df, location, total_num_of_ES):
+    nt_precentage = {'A': 0, 'C': 0, 'G': 0, 'T':0}
+    for nt in nt_precentage.keys():
+        perc = (nt_count_df[f'{location}_{nt}'].sum() / (total_num_of_ES))
+        nt_precentage[nt] = perc
+    return nt_precentage
+
+#create json file summarizing key statistics of he passed rfeads
+def create_json(passed_df, condition_df):
+
+    # init general data dictonary 
+    json_data = {"sample_id: ": sample_id}
+    
+    # add total number of filtered reads
+    json_data["number of passed reads: "] = int(condition_df['Passed_All'].sum())
+
+    # average es number
+    average_num_of_ES = passed_df['number_of_ES'].mean()
+    json_data["average number of ES: "] = average_num_of_ES
+
+    # get sample ES motif
+    total_num_of_ES = passed_df['number_of_ES'].sum()
+    for loc in motif_location:
+        nt_count_df = passed_df[[f'{loc}_{nt}' for nt in ['A', 'C', 'G', 'T']]]
+        nt_precentage =sample_motif(nt_count_df, loc, total_num_of_ES)
+        json_data[f'{loc}_motif: '] = nt_precentage
+
+    # Get the list of condition columns
+    condition_df = condition_df.drop(columns=['Read_ID', 'Passed_All'])
+
+    # get values of parameters for each column and remove it from the name
+    parameters_values = {}
+    for col in condition_df.columns:
+        if (col == 'Edited'):
+            continue
+        # extract param's value
+        param_value = col.rsplit('_', 1)[-1]
+        # rename the columns to discard the param's value
+        column_name = col.rsplit('_', 1)[0]
+        condition_df.rename(columns={col: column_name}, inplace=True)
+        # add to the parameter's values
+        parameters_values[column_name] = param_value
+    json_data["Parameters' Values: "] = parameters_values
+
+    # Create dictionary with the sum of reads which passed each condition
+    each_condition = condition_df.sum().astype(int).to_dict()
+    json_data["number of passed reads for each condition"] = each_condition
+
+    return json_data
+
+
+# count number of upstream + downstrean nt for each ES, returns a dictionary with the sum for each type of motif nt 
 def motifs_count(read_seq, editing_sites_map):
     es_positions = list(editing_sites_map.keys())
-    nt_counter = {'A': 0, 'G': 0, 'G': 0, 'T':0}
+    nt_counter = {'A': 0, 'C': 0, 'G': 0, 'T':0}
     read_len = len(read_seq)
     counter_dict = {}
     motif_location = ['upstream', 'downstream']
@@ -115,8 +189,9 @@ def motifs_count(read_seq, editing_sites_map):
     for motif_location in counter_dict.keys():
         for nt, count in counter_dict[motif_location].items():
             data[f'{motif_location}_{nt}'] = count
-    return data #pd.DataFrame([data])
+    return data 
 
+# calculat and returns average phred score for all of the ES, and averge of al distances netween adjactent ES
 def es_statistics(editing_sites_map):
     phred_scores = editing_sites_map.values()
     phred_score_avg = sum(phred_scores) / len(phred_scores)
@@ -124,8 +199,8 @@ def es_statistics(editing_sites_map):
     es_positions.sort()
     avg_lambda_func =  lambda es_positions: sum(abs(es_positions[i+1] - es_positions[i]) for i in range(0, len(es_positions) - 1)) / len(es_positions)
     avg_es_distance = avg_lambda_func(es_positions)
-
-    return phred_score_avg, avg_es_distance
+    num_of_es = len(editing_sites_map)
+    return phred_score_avg, avg_es_distance, num_of_es
 
     
 
@@ -162,10 +237,10 @@ for read in clusters_df.itertuples():
 
     # if read passed all parameteres add a row to the passed_statisctics
     if passed_all_conditions and out_passed:
-        avg_phred_score, avg_es_distance = es_statistics(editing_sites_map)
+        avg_phred_score, avg_es_distance, num_of_es = es_statistics(editing_sites_map)
         motifs_count_data = motifs_count(read.Read_Sequence, editing_sites_map)
-        es_statistics_data = {'average_es_phred_score': avg_phred_score, 'average_adjacent_es_distance': avg_es_distance}
-        all_data = {}
+        es_statistics_data = {'number_of_ES': num_of_es, 'average_es_phred_score': avg_phred_score, 'average_adjacent_es_distance': avg_es_distance}
+        all_data = {"Read_ID": read[0]}
         all_data.update(es_statistics_data)
         all_data.update(motifs_count_data)
         passed_reads_data.append(all_data)
@@ -173,19 +248,24 @@ for read in clusters_df.itertuples():
     if out_analysis:
         condition_analysis_rows.append([read[0]] + [passed_all_conditions] + [edited] + conditions_list)
 
+
+# WRITE OUTPUT
+#CREATE DATA FRAMES
+passed_df = pd.DataFrame(passed_reads_data)
+
+condition_header = ['Read_ID', 'Passed_All', 'Edited', 'Min_Editing_Sites_' + str(args.min_editing_sites), 'Min_Editing_to_Total_MM_Fraction_' + str(args.min_editing_fraction), 'Min_Editing_Phred_Score_' + str(args.min_phred_score), 'Min_Editing_to_Read_Length_Ratio_' + str(args.min_es_length_ratio), 'Min_Cluster_Length_to_Read_Length_Ratio_' + str(args.min_cluster_length_ratio)]
+condition_df = pd.DataFrame(condition_analysis_rows, columns=condition_header)
+
 if out_passed:
     # Write all the rows to the passed CSV file
-    passed_df = pd.DataFrame(passed_reads_data)
     passed_df.to_csv(passed_csv_path)
-
 if out_analysis:
     # Write all the rows to the condition_analysis CSV file
-    with open(condition_analysis_path, 'w', newline='') as condition_analysis_csv:
-        csv_writer_condition_analysis = csv.writer(condition_analysis_csv)
-        # Write header
-        condition_header = ['Read_ID', 'Passed_All', 'Edited', 'Min_Editing_Sites_' + str(args.min_editing_sites), 'Min_Editing_to_Total_MM_Fraction_' + str(args.min_editing_fraction), 'Min_Editing_Phred_Score_' + str(args.min_phred_score), 'Min_Editing_to_Read_Length_Ratio_' + str(args.min_es_length_ratio), 'Min_Cluster_Length_to_Read_Length_Ratio_' + str(args.min_cluster_length_ratio)]
-        csv_writer_condition_analysis.writerow(condition_header)
-        # Write all rows
-        csv_writer_condition_analysis.writerows(condition_analysis_rows)
+    condition_df.to_csv(condition_analysis_path)
+if out_json:
+    json_data = create_json(passed_df, condition_df)
+    # write the data to a json file
+    with open(json_summary_path, 'w') as f:
+        json.dump(json_data, f, indent = 4)
 
 
